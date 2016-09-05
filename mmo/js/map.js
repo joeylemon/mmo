69a9b1082c80eb999dceb0897d66f33edefae90b
finelime <joeyclemon@gmail.com>
2016-09-05 13:15:46 -0400

    Clean up map rendering

--- mmo/js/map.js
+++ mmo/js/map.js
@@ -3,64 +3,67 @@ var map = undefined;
 
 var maxX = 0;
 var maxY = 0;
+var maxSet = false;
 
-var tileset;
+var tilesetImg;
 var set;
 
 
 function loadMap(){
-	if (!map)
-		return;
-	init();
+	if(map){
+		init();
+	}
 }
 
 function loadWorld(){
-	tileset = new Image();
-	tileset.src = "js/tilesheet.png";
+	tilesetImg = new Image();
+	tilesetImg.src = "js/tilesheet.png";
 	
 	loadJSONMap("js/world.json", loadMap);
 }
 
 function renderCell(x, y, id){
-	if (id == 0)
-		return; // Skip empty cells
-	
-	if(set == undefined){
-		// Search right tileset image
-		for (var i = 0; i < map.tilesets.length; ++i){
-			if (map.tilesets[i].firstgid <= id)
-				set = map.tilesets[i];
-		}
+	if(id != 0){
+		var tile_id = id - set.firstgid;
+		var set_width = set.imagewidth / set.tilewidth;
+		var tile_x = tile_id % set_width
+		var tile_y = Math.floor(tile_id / set_width);
+		
+		ctx.drawImage(
+			tilesetImg, tile_x * set.tilewidth, tile_y * set.tileheight,
+			set.tilewidth, set.tileheight,
+			x * set.tilewidth, y * set.tileheight,
+			set.tilewidth, set.tileheight
+		);
 	}
-	
-	var tile_id = id - set.firstgid;
-	var set_width = set.imagewidth / set.tilewidth;
-	var tile_x = tile_id % set_width
-	var tile_y = Math.floor(tile_id / set_width);
-	
-	ctx.drawImage(
-		tileset, tile_x * set.tilewidth, tile_y * set.tileheight,
-		set.tilewidth, set.tileheight,
-		x * set.tilewidth, y * set.tileheight,
-		set.tilewidth, set.tileheight
-	);
 }
 
 function init(){
-	for (var layer = 0; layer < map.layers.length; ++layer){
+	for(var layer = 0; layer < map.layers.length; ++layer){
 		for (var cell = 0; cell < map.layers[layer].data.length; ++cell){
 			var x = cell % map.layers[layer].width;
 			var y = Math.floor((cell - x) / map.layers[layer].height);
 			renderCell(x, y, map.layers[layer].data[cell]);
 			
-			if((x * map.tilewidth) > maxX){
-				maxX = (x * map.tilewidth);
-			}
-			if((y * map.tileheight) > maxY){
-				maxY = (y * map.tileheight);
+			if(!maxSet){
+				if((x * map.tilewidth) > maxX){
+					maxX = (x * map.tilewidth);
+				}
+				if((y * map.tileheight) > maxY){
+					maxY = (y * map.tileheight);
+				}
 			}
 		}
 	}
+	maxSet = true;
+}
+
+function loadJSONMap(url, callback){
+	$.getJSON(url, function(json){
+		map = json;
+		set = map.tilesets[0];
+		callback();
+	});
 }
 
 function getMaxX(){
@@ -69,27 +72,4 @@ function getMaxX(){
 
 function getMaxY(){
 	return maxY;
-}
-
-function loadJSONMap(url, callback){
-	$.getJSON(url, function(json){
-		map = json;
-		var num_of_loaded = 0
-
-		// Random checks if it is a valid tiled generated map
-		if(!(map.layers && map.orientation && map.tilesets))
-			callback();
-
-
-		for(var i = 0; i < map.tilesets.length; ++i){
-			map.tilesets[i].img = new Image();
-			map.tilesets[i].img.onload = function(){
-				if (++num_of_loaded == map.tilesets.length){
-					init();
-				}
-			}
-			map.tilesets[i].img.src = map.tilesets[i].image;
-		}
-		callback();
-	});
 }
