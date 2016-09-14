1484935025c625089fa83916c811e72dfcea28b4
finelime <joeyclemon@gmail.com>
2016-09-14 13:25:05 -0400

    Update database with new user info

--- mmo/server.js
+++ mmo/server.js
@@ -26,63 +26,18 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.post('/js/login.js', function(req, res){
-	var con = newConnection();
-	
 	var username = req.body.username;
 	var password = req.body.password;
 	
-	con.query("SELECT * FROM users WHERE username='" + username + "'", function(err, rows){
-		if(err) throw err;
-		
-		if(rows.length > 0){
-			var hash_pass = rows[0].password;
-			if(hashing.compareSync(password, hash_pass)){
-				res.end(JSON.stringify(rows[0]));
-			}else{
-				res.end("bad password");
-			}
-		}else{
-			res.end("bad username");
-		}
-	});
-	
-	con.end(function(err){
-		
-	});
+	perform("login", {res: res, username: username, password: password});
 });
 
 app.post('/js/newuser.js', function(req, res){
-	var con = newConnection();
-	
 	var username = req.body.username;
 	var password = req.body.password;
 	var uuid = req.body.uuid;
 	
-	var hash_pass = hashing.hashSync(password);
-	
-	con.query("SELECT * FROM users WHERE username='" + username + "'", function(err, rows){
-		if(err) throw err;
-		
-		if(rows.length == 0){
-			var user = {
-				username: username,
-				password: hash_pass,
-				uuid: uuid,
-				level: 1,
-				inv: '{\"armor\":\"clotharmor\"}',
-				pos: '{\"x\":0,\"y\":0}'
-			};
-			con.query('INSERT INTO users SET ?', user, function(err, res){
-				con.end(function(err){
-					if(err) throw err;
-				});
-				if(err) throw err;
-			});
-			res.end("good username");
-		}else{
-			res.end("bad username");
-		}
-	});
+	perform("newuser", {res: res, username: username, password: password, uuid: uuid});
 });
 
 http.listen(settings.port, function(){
@@ -121,6 +76,8 @@ io.on('connection', function(socket){
 				players: newarray
 			};
 			socket.emit('msg', response);
+		}else if(data.type == "update"){
+			users[getUserIndex(socket.id)][data.property] = data.newvalue;
 		}else{
 			broadcast(data);
 		}
@@ -128,6 +85,8 @@ io.on('connection', function(socket){
 	
 	socket.on('disconnect', function() {
 		console.log("Client disconnected (" + ip + ")");
+		var user = users[getUserIndex(socket.id)];
+		perform("update", {username: user.username, level: user.level, inv: user.inv, pos: user.pos});
 		removeUser(socket.id);
 	});
 });
@@ -135,6 +94,10 @@ io.on('connection', function(socket){
 
 
 /* File functions */
+function broadcast(data){
+	io.emit('msg', data);
+}
+
 function addUser(data, id, socket){
 	data["id"] = id;
 	data["socket"] = socket;
@@ -142,7 +105,7 @@ function addUser(data, id, socket){
 	console.log(data.username + " has joined.");
 }
 
-function removeUser(id){
+function getUserIndex(id){
 	var index = -1;
 	for(var i = 0; i < users.length; i++){
 		var user = users[i];
@@ -151,6 +114,11 @@ function removeUser(id){
 			break;
 		}
 	}
+	return index;
+}
+
+function removeUser(id){
+	var index = getUserIndex(id)
 	if(index > -1){
 		console.log(users[index].username + " has left.");
 		users.splice(index, 1);
@@ -159,11 +127,7 @@ function removeUser(id){
 	}
 }
 
-function broadcast(data){
-	io.emit('msg', data);
-}
-
-function newConnection(){
+function database(){
 	var con = mysql.createConnection({
 		host: "107.180.40.107",
 		user: "joeylemon",
@@ -179,4 +143,87 @@ function newConnection(){
 	});
 	
 	return con;
+}
+
+function perform(task, info){
+	if(task == "login"){
+		var con = database();
+		
+		var res = info.res;
+		var username = info.username;
+		var password = info.password;
+		
+		con.query("SELECT * FROM users WHERE username='" + username + "'", function(err, rows){
+			if(err) throw err;
+			
+			if(rows.length > 0){
+				var hash_pass = rows[0].password;
+				if(hashing.compareSync(password, hash_pass)){
+					res.end(JSON.stringify(rows[0]));
+				}else{
+					res.end("bad password");
+				}
+			}else{
+				res.end("bad username");
+			}
+		});
+		
+		con.end(function(err){
+			if(err) throw err;
+		});
+	}else if(task == "newuser"){
+		var con = database();
+	
+		var res = info.res;
+		var username = info.username;
+		var password = info.password;
+		var uuid = info.uuid;
+		
+		var hash_pass = hashing.hashSync(password);
+		
+		con.query("SELECT * FROM users WHERE username='" + username + "'", function(err, rows){
+			if(err) throw err;
+			
+			if(rows.length == 0){
+				var user = {
+					username: username,
+					password: hash_pass,
+					uuid: uuid,
+					level: 1,
+					inv: '{\"armor\":\"clotharmor\"}',
+					pos: '{\"x\":0,\"y\":0}'
+				};
+				con.query("INSERT INTO users SET ?", user, function(err, res){
+					con.end(function(err){
+						if(err) throw err;
+					});
+					if(err) throw err;
+				});
+				res.end("good username");
+			}else{
+				res.end("bad username");
+			}
+		});
+	}else if(task == "update"){
+		var con = database();
+	
+		var username = info.username;
+		var level = info.level;
+		var pos = info.pos;
+		var inv = info.inv;
+		
+		var hash_pass = hashing.hashSync(password);
+		
+		var updated = {
+			level: level,
+			pos: JSON.stringify(pos),
+			inv: JSON.stringify(inv)
+		};
+		con.query("UPDATE users SET ? WHERE username='" + username + "'", updated, function(err, rows){
+			if(err) throw err;
+		});
+		con.end(function(err){
+			if(err) throw err;
+		});
+	}
 }
